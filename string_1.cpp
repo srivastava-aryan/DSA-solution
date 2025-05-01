5904bbcaca728544e698418c8eabd4a98a998a62
srivastava-aryan <aru93295@gmail.com>
2025-05-01 17:36:46 +0530

    new file for string

---
+++ string_1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str = "first";
+    string str2 = "second";
+    for(int i = 0; i < str.length() ; i++){
+        cout<<str[i]<<endl;
+    }
+    cout<<str+str2<<endl;
+    return 0;
+}
