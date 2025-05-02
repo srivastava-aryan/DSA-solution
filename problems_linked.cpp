b3672d17d3b83588e2ef75c93f77499bde518904
srivastava-aryan <aru93295@gmail.com>
2025-05-02 22:18:24 +0530

    new problem

--- problems_linked.cpp
+++ problems_linked.cpp
@@ -10,6 +10,23 @@ struct Node{
     }
 };
 
+Node* reverseLinked(Node* head){
+    if(head==NULL||head->next==NULL){
+        return head;
+    }
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp!=NULL){
+        Node* front  = temp->next;
+
+        temp->next=prev;
+
+        prev= temp;
+        temp = front;
+    }
+    return prev;
+}
+
 Node* addTwoNumbers(Node* l1, Node* l2){    //to add two corresponding linked lists
     Node* dummyhead = new Node(-1);        //dummyhead is used to point the head of the linked list representing the sum of two linked lists
     Node* curr = dummyhead;
@@ -33,4 +50,63 @@ Node* addTwoNumbers(Node* l1, Node* l2){    //to add two corresponding linked li
         curr->next = newNode;
     }
     return dummyhead->next;
+}
+
+Node* addTwoNumbersEnd(Node* l1, Node* l2){
+    l1 = reverseLinked(l1);
+    l2 = reverseLinked(l2);
+    Node* dummyNode = new Node(-1);
+    Node* curr = dummyNode;
+    Node* t1 = l1;
+    Node* t2 = l2;
+    int carry= 0;
+    while(t1!=NULL||t2!=NULL){
+        int sum = carry;
+        if(t1)sum+=t1->data;
+        if(t2)sum+=t2->data;
+        Node* newNode = new Node(sum%10);
+        carry = sum/10;
+        curr->next = newNode;
+        curr= curr->next;
+
+        if(t1)t1 = t1->next;
+        if(t2)t2 = t2->next;
+    }
+    if(carry){
+        Node* newNode = new Node(carry);
+        curr->next = newNode;
+    }
+    return dummyNode->next;
+}
+int LengthofLL(Node* head){     //////////to count no. of nodes
+    int cnt = 0;
+    Node* temp = head;
+    while(temp){
+        temp=temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+Node* rotateRight(Node* head, int k) {  // to rotate a linked-list k times
+    if(head==NULL||head->next==NULL)return head;
+    int cnt = LengthofLL(head);
+    int r = k%cnt;
+    while(r){
+        Node* temp = head->next;
+        Node* prev = head;
+        while(temp!=NULL){
+            if(temp->next==NULL){
+                temp->next=head;
+                head=temp;
+                prev->next=NULL;
+                break;
+            }
+            temp=temp->next;
+            prev=prev->next;
+        }
+        r--;
+    }
+    return head;
+    
 }
--- string_1.cpp
+++ string_1.cpp
@@ -1,12 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    string str = "first";
-    string str2 = "second";
-    for(int i = 0; i < str.length() ; i++){
-        cout<<str[i]<<endl;
+// void substringRange(string str){    // to find all the substrings 
+//     int n = str.length();
+//     for(int i = 0;i<n;i++){
+//         for(int j = 1;j<=n-i;j++){
+//             cout<<str.substr(i,j)<<endl;
+//         }
+//     }
+// }
+
+// int toDigit(char ch){return (ch-'0');}
+
+int sumofSubstring(string str){         // to find the sum of all substrings
+    vector<int> v;
+    int n = str.length();
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=n-i;j++){                
+            string sub = (str.substr(i,j));     
+            int x = stoi(sub);            // stoi is used to convert string to integer
+            v.push_back(x);
+        }
     }
-    cout<<str+str2<<endl;
+    int res = accumulate(v.begin(),v.end(),0);      // accumulate is used to find the sum of all elements in the vector
+    return res;
+}
+
+int main(){
+    string str = "1234";
+    // substringRange(str); 
+    cout<<"sum is :"<<sumofSubstring(str)<<" ";
     return 0;
 }
